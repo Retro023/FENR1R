089e3e9f1bb1d3114b3c5b524339669aa1577494
MuteAvery <107277836+Retro023@users.noreply.github.com>
2025-11-22 15:52:59 +0000

    Refactor get_ip_address and error handling

--- scripts/Network.py
+++ scripts/Network.py
@@ -11,16 +11,18 @@ network_interfaces = []
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        return socket.inet_ntoa(fcntl.ioctl(
-            s.fileno(),
-            0x8915,  # SIOCGIFADDR
-            struct.pack('256s', ifname.encode('utf-8')[:15])
-        )[20:24])
+        return socket.inet_ntoa(
+            fcntl.ioctl(
+                s.fileno(),
+                0x8915,  # SIOCGIFADDR
+                struct.pack("256s", ifname.encode("utf-8")[:15]),
+            )[20:24]
+        )
     except OSError:
         return None
 
 
-# scan for interfaces using the net folder 
+# scan for interfaces using the net folder
 def scan_network_interfaces():
     try:
         interfaces = os.listdir("/sys/class/net/")
@@ -35,24 +37,24 @@ def scan_network_interfaces():
         try:
             with open(os.path.join(interface_path, "operstate"), "r") as file:
                 up = file.read().strip() == "up"
-        except:
-            up = None
+        except Exception as e:
+            print(f"Error getting interface status: {e}")
+            up = "No"
 
         # Get MAC address
         try:
             with open(os.path.join(interface_path, "address"), "r") as file:
                 mac = file.read().strip()
-        except:
-            mac = None
+        except Exception as e:
+            print(f"Error getting Mac address: {e}")
+
+            mac = "Empty"
 
         # Get IPv4 address
         ip = get_ip_address(interface)
 
-        network_interfaces.append({
-            "Name": interface,
-            "IP": [ip] if ip else "no ipv4",
-            "MAC": mac,
-            "Up": up
-        })
+        network_interfaces.append(
+            {"Name": interface, "IP": [ip] if ip else "no ipv4", "MAC": mac, "Up": up}
+        )
 
     return network_interfaces
