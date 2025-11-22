d6ab93a7f1a890a116de68964f3d53482c4dfbfb
MuteAvery <107277836+Retro023@users.noreply.github.com>
2025-11-22 15:52:32 +0000

    Refactor SUID.py for better readability
    Refactor comments and improve code formatting for clarity.

--- scripts/SUID.py
+++ scripts/SUID.py
@@ -1,12 +1,11 @@
-import subprocess
-import os 
+import os
 import stat
 from time import sleep
 
-# list of known suids for priv esc 
+# list of known suids for priv esc
 # match the binary to a link to a known privesv vector: i,e   Binary1:exploit.com/binary1
 known_privEscs = {
-"aa-exec": "https://gtfobins.github.io/gtfobins/aa-exec/#suid",
+    "aa-exec": "https://gtfobins.github.io/gtfobins/aa-exec/#suid",
     "ab": "https://gtfobins.github.io/gtfobins/ab/#suid",
     "agetty": "https://gtfobins.github.io/gtfobins/agetty/#suid",
     "alpine": "https://gtfobins.github.io/gtfobins/alpine/#suid",
@@ -128,70 +127,73 @@ known_privEscs = {
     "msgfilter": "https://gtfobins.github.io/gtfobins/msgfilter/#suid",
     "msgmerge": "https://gtfobins.github.io/gtfobins/msgmerge/#suid",
     "msguniq": "https://gtfobins.github.io/gtfobins/msguniq/#suid",
-    "tar":  "https://gtfobins.github.io/gtfobins/tar/#limited-suid"
+    "tar": "https://gtfobins.github.io/gtfobins/tar/#limited-suid",
 }
 # add more
 
 
-#Colours
+# Colours
 blue = "\033[0;34m"
 cyan = "\033[0;36m"
 purp = "\033[0;35m"
 END = "\033[0m"
 
 
-
-
 # scan for Suids then print the suids to user and tell the user if binary has a known suid privesc
-# Save the suid files to a list and the known vectors another list to then print them to the output file 
+# Save the suid files to a list and the known vectors another list to then print them to the output file
 def scan_suids():
-    # suid file list 
-    Suid_files = [] 
-    
-    # list of vectors to privesc 
+    # suid file list
+    Suid_files = []
+
+    # list of vectors to privesc
     vectors = []
-    
+
     # total ammount of files Scanned
     total = 0
 
     # walk thru the system file and gather files
-    for root, dirs, files in os.walk("/", topdown=True,followlinks=False):
+    for root, dirs, files in os.walk("/", topdown=True, followlinks=False):
         for name in files:
             # give user update on scan so they do not think it crashed
             total += 1
-            print(f"{purp}Scanning files for sticky bits... Checked: {total} {END}", end="\r")
-            path = os.path.join(root,name)
-            
+            print(
+                f"{purp}Scanning files for sticky bits... Checked: {total} {END}",
+                end="\r",
+            )
+            path = os.path.join(root, name)
+
             # check if file has sticky bit (simple error check to see if user has perms and file exists)
             try:
                 st = os.stat(path)
-            except (PermissionError, FileNotFoundError,OSError):
+            except (PermissionError, FileNotFoundError, OSError):
                 continue
             if st.st_mode & stat.S_ISUID:
                 Suid_files.append(path)
-        
-    # if suids are found then print them in a easy to follow neat format
-    if Suid_files:
-        print("\nSUIDs FOUND!!")
-        print("="*30+"|")
-        sleep(0.1)
-        for i in Suid_files:
-            
-            # if the binary is in the known_privEscs then print the bianry with the link
-            filename = os.path.basename(i)
-            if filename in known_privEscs:
-                print(f"{cyan}Known Privesc vector found!{END}")
-                print(">",filename,":", known_privEscs[filename])
-                
-                #save the privesc vector to the vectors list for output file 
-                vectors.append(known_privEscs[filename])
+
+    try:
+        # if suids are found then print them in a easy to follow neat format
+        if Suid_files:
+            print("\nSUIDs FOUND!!")
+            print("=" * 30 + "|")
+            sleep(0.1)
+            for i in Suid_files:
+                # if the binary is in the known_privEscs then print the bianry with the link
+                filename = os.path.basename(i)
+                if filename in known_privEscs:
+                    print(f"{cyan}Known Privesc vector found!{END}")
+                    print(">", filename, ":", known_privEscs[filename])
+
+                    # save the privesc vector to the vectors list for output file
+                    vectors.append(known_privEscs[filename])
+                    sleep(0.2)
+
+                # print Suids every 0.2 seconds
+                print(i)
                 sleep(0.2)
-            
-            #print Suids every 0.2 seconds 
-            print(i)
-            sleep(0.2)
-        print("="*30+"|")
+            print("=" * 30 + "|")
+            return Suid_files, vectors
 
+    except Exception as e:
+        print(f"Error occured while checking for SUID vectors\nError: {e}")
+        vectors = "Error occured"
         return Suid_files, vectors
-
-
